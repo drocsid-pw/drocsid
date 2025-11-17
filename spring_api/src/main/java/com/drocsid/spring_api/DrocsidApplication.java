a3c1593d90ef4fb23b72d8741fd1ff3ad019cb26
mcieslik <01187214@pw.edu.pl>
2025-11-17 11:50:40 +0100

    Created DrocsidApp

---
+++ spring_api/src/main/java/com/drocsid/spring_api/DrocsidApplication.java
@@ -0,0 +1,10 @@
+package com.drocsid.spring_api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrocsidApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DrocsidApplication.class, args);
+    }
+}
