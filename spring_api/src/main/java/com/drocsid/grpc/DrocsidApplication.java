63c0ded85dcdbda072a9e551a75994acd7437d46
mcieslik <01187214@pw.edu.pl>
2025-11-19 12:32:57 +0100

    Moved to other directory

--- spring_api/src/main/java/com/drocsid/grpc/DrocsidApplication.java
+++ spring_api/src/main/java/com/drocsid/grpc/DrocsidApplication.java
@@ -1,6 +1,8 @@
-package com.drocsid.spring_api;
+package com.drocsid.grpc;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class DrocsidApplication {
