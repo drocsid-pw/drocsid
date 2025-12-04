42104276789c797e681eabab8a371c39f34801b0
mcieslik <01187214@pw.edu.pl>
2025-12-04 13:59:17 +0100

    Added channel methods

---
+++ spring_grpc/src/main/java/com/drocsid/grpc/service/ChannelService.java
@@ -0,0 +1,121 @@
+package com.drocsid.grpc.service;
+import com.drocsid.grpc.core_requests.create.CoreCreateChannelRequest;
+import com.drocsid.grpc.core_requests.update.CoreUpdateChannelRequest;
+import com.drocsid.grpc.mock_classes.CoreClient;
+import com.drocsid.grpc.proto.*;
+import com.drocsid.grpc.proto.CreateChannelRequest;
+import com.drocsid.grpc.proto.UpdateChannelRequest;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@GrpcService
+public class ChannelService extends ChannelServiceGrpc.ChannelServiceImplBase {
+
+    private final CoreClient coreClient;
+
+    public ChannelService(CoreClient coreClient) {
+        this.coreClient = coreClient;
+    }
+
+    @Override
+    public void createChannel(CreateChannelRequest request, StreamObserver<ResponseMessage> responseObserver) {
+        try {
+            coreClient.createChannel(new CoreCreateChannelRequest(request));
+
+            ResponseMessage response = ResponseMessage
+                    .newBuilder()
+                    .setText("Channel created successfully.")
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+        }
+        catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void getChannel(ChannelId channelId, StreamObserver<Channel> responseObserver) {
+        try {
+            Channel channel = coreClient.getChannel(new BigInteger(channelId.getUserId()),
+                new BigInteger(channelId.getId()));
+
+            responseObserver.onNext(channel);
+            responseObserver.onCompleted();
+
+        }
+        catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void updateChannel(UpdateChannelRequest request,
+                           StreamObserver<ResponseMessage> responseObserver) {
+        try {
+            coreClient.updateChannel(new CoreUpdateChannelRequest(request));
+
+            ResponseMessage response = ResponseMessage
+                    .newBuilder()
+                    .setText("Channel updated successfully.")
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+        }
+        catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void deleteChannel(ChannelId channelId, StreamObserver<ResponseMessage> responseObserver) {
+        try {
+            coreClient.deleteChannel(new BigInteger(channelId.getUserId()), new BigInteger(channelId.getId()));
+
+            ResponseMessage response = ResponseMessage
+                    .newBuilder()
+                    .setText("Channel deleted successfully.")
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+        }
+        catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void getLastMessage(UserId userId, StreamObserver<Message> responseObserver) {
+        try {
+            Message message = coreClient.getLastMessage(new BigInteger(userId.getId()));
+
+            responseObserver.onNext(message);
+            responseObserver.onCompleted();
+
+        }
+        catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void getAllMessages(UserId userId, StreamObserver<MessageList> responseObserver) {
+        try {
+            List<Message> messages = coreClient.getAllMessages(new BigInteger(userId.getId()));
+
+            MessageList list = MessageList.newBuilder().addAllMessages(messages).build();
+            responseObserver.onNext(list);
+            responseObserver.onCompleted();
+
+        }
+        catch (Exception e) {
+            responseObserver.onError(e);
+        }
+    }
+}
