42ba04c4b4972b3a584cb34bdbb4f7bc99218e11
mcieslik <01187214@pw.edu.pl>
2025-12-07 12:05:31 +0100

    Removed unnecessary fields. Fields will be moved to separate requests.

--- spring_grpc/src/main/java/com/drocsid/grpc/core_requests/update/CoreUpdateGuildRequest.java
+++ spring_grpc/src/main/java/com/drocsid/grpc/core_requests/update/CoreUpdateGuildRequest.java
@@ -1,6 +1,5 @@
 package com.drocsid.grpc.core_requests.update;
 
-import com.drocsid.grpc.proto.Channel;
 import com.drocsid.grpc.proto.UpdateGuildRequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,8 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,15 +17,11 @@ public class CoreUpdateGuildRequest {
     private BigInteger id;
     private String name;
     private String icon;
-    private BigInteger ownerId;
-    private List<Channel> channels;
 
     public CoreUpdateGuildRequest(UpdateGuildRequest request) {
         userId = new BigInteger(request.getUserId());
         id = new BigInteger(request.getId());
         name = request.getName();
         icon = request.getIcon();
-        ownerId = new BigInteger(request.getOwnerId());
-        channels = new ArrayList<>(request.getChannelsList());
     }
 }
