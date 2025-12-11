21bcd2ca8d1d9cc908cafbeab18de20ecc9e9115
mcieslik <01187214@pw.edu.pl>
2025-12-11 14:52:08 +0100

    Rebuild channelService to be suitable to new proto file

--- spring_grpc/src/main/java/com/drocsid/grpc/service/ChannelService.java
+++ spring_grpc/src/main/java/com/drocsid/grpc/service/ChannelService.java
@@ -1,9 +1,13 @@
 package com.drocsid.grpc.service;
+import com.drocsid.grpc.auth.JwtAuthService;
+import com.drocsid.grpc.core_requests.channel.CoreGetMessagesRequest;
 import com.drocsid.grpc.core_requests.channel.CoreUpdateChannelRequest;
 import com.drocsid.grpc.core_requests.channel.CoreCreateMessageRequest;
 import com.drocsid.grpc.mock_classes.CoreClient;
 import com.drocsid.grpc.proto.*;
 import com.drocsid.grpc.proto.UpdateChannelRequest;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
@@ -14,67 +18,144 @@ import java.util.List;
 public class ChannelService extends ChannelServiceGrpc.ChannelServiceImplBase {
 
     private final CoreClient coreClient;
+    private final JwtAuthService jwtAuthService;
 
-    public ChannelService(CoreClient coreClient) {
+    public ChannelService(CoreClient coreClient, JwtAuthService jwtAuthService) {
         this.coreClient = coreClient;
+        this.jwtAuthService = jwtAuthService;
     }
 
     @Override
-    public void getChannelInfo(ChannelId channelId, StreamObserver<ChannelInfo> responseObserver) {
+    public void getChannel(ChannelInfoReq request, StreamObserver<Channel> responseObserver) {
         try {
-            ChannelInfo channel = coreClient.getChannelInfo(new BigInteger(channelId.getUserId()),
-                new BigInteger(channelId.getId()));
+            String userId = jwtAuthService.checkAuth(request.getToken());
+            Channel channel = coreClient.getChannel(new BigInteger(userId),
+                new BigInteger(request.getChannelId()));
 
             responseObserver.onNext(channel);
             responseObserver.onCompleted();
 
         }
-        catch (Exception e) {
+        catch (StatusRuntimeException e) {
             responseObserver.onError(e);
+        }
+        catch (Exception e) {
+            responseObserver.onError(
+                    Status.INTERNAL
+                            .withDescription("Unexpected server error: " + e.getMessage())
+                            .withCause(e)
+                            .asRuntimeException()
+            );
         }
     }
 
     @Override
     public void updateChannel(UpdateChannelRequest request,
-                           StreamObserver<ResponseMessage> responseObserver) {
+                           StreamObserver<Channel> responseObserver) {
         try {
-            coreClient.updateChannel(new CoreUpdateChannelRequest(request));
+            String userId = jwtAuthService.checkAuth(request.getToken());
+            Channel channel = coreClient.updateChannel(new CoreUpdateChannelRequest(userId, request));
+
+            responseObserver.onNext(channel);
+            responseObserver.onCompleted();
+
+        }
+        catch (StatusRuntimeException e) {
+            responseObserver.onError(e);
+        }
+        catch (Exception e) {
+            responseObserver.onError(
+                    Status.INTERNAL
+                            .withDescription("Unexpected server error: " + e.getMessage())
+                            .withCause(e)
+                            .asRuntimeException()
+            );
+        }
+    }
+
+
+    @Override
+    public void deleteChannel(ChannelInfoReq request, StreamObserver<ResponseMessage> responseObserver) {
+        try {
+            String userId = jwtAuthService.checkAuth(request.getToken());
+            coreClient.deleteChannel(new BigInteger(userId), new BigInteger(request.getChannelId()));
 
             ResponseMessage response = ResponseMessage
                     .newBuilder()
-                    .setText("Channel updated successfully.")
+                    .setText("Channel deleted successfully.")
                     .build();
             responseObserver.onNext(response);
             responseObserver.onCompleted();
 
         }
-        catch (Exception e) {
+        catch (StatusRuntimeException e) {
             responseObserver.onError(e);
+        }
+        catch (Exception e) {
+            responseObserver.onError(
+                    Status.INTERNAL
+                            .withDescription("Unexpected server error: " + e.getMessage())
+                            .withCause(e)
+                            .asRuntimeException()
+            );
         }
     }
 
-    @Override
-    public void createMessage(CreateMessageRequest request, StreamObserver<ResponseMessage> responseObserver) {
-        try {
-            coreClient.createMessage(new CoreCreateMessageRequest(request));
 
-            ResponseMessage response = ResponseMessage
-                    .newBuilder()
-                    .setText("Message created successfully.")
-                    .build();
-            responseObserver.onNext(response);
+    @Override
+    public void getMessages(MessageBucketReq request, StreamObserver<MessageList> responseObserver) {
+        try {
+            String userId = jwtAuthService.checkAuth(request.getReq().getToken());
+            List<Message> messages = coreClient.getMessages(new CoreGetMessagesRequest(userId, request));
+            MessageList messageList = MessageList.newBuilder().addAllMessages(messages).build();
+
+            responseObserver.onNext(messageList);
             responseObserver.onCompleted();
 
         }
-        catch (Exception e) {
+        catch (StatusRuntimeException e) {
             responseObserver.onError(e);
+        }
+        catch (Exception e) {
+            responseObserver.onError(
+                    Status.INTERNAL
+                            .withDescription("Unexpected server error: " + e.getMessage())
+                            .withCause(e)
+                            .asRuntimeException()
+            );
+        }
+    }
+
+
+    @Override
+    public void createMessage(CreateMessageReq request, StreamObserver<Message> responseObserver) {
+        try {
+            String userId = jwtAuthService.checkAuth(request.getReq().getToken());
+            Message message = coreClient.createMessage(new CoreCreateMessageRequest(userId, request));
+
+            responseObserver.onNext(message);
+            responseObserver.onCompleted();
+
+        }
+        catch (StatusRuntimeException e) {
+            responseObserver.onError(e);
+        }
+        catch (Exception e) {
+            responseObserver.onError(
+                    Status.INTERNAL
+                            .withDescription("Unexpected server error: " + e.getMessage())
+                            .withCause(e)
+                            .asRuntimeException()
+            );
         }
     }
 
     @Override
-    public void deleteMessage(MessageId messageId, StreamObserver<ResponseMessage> responseObserver) {
+    public void deleteMessage(MessageReq request, StreamObserver<ResponseMessage> responseObserver) {
         try {
-            coreClient.deleteMessage(new BigInteger(messageId.getUserId()), new BigInteger(messageId.getId()));
+            String userId = jwtAuthService.checkAuth(request.getReq().getToken());
+            coreClient.deleteMessage(new BigInteger(userId),
+                new BigInteger(request.getMessageReq().getMessageId()));
 
             ResponseMessage response = ResponseMessage
                     .newBuilder()
@@ -84,39 +165,16 @@ public class ChannelService extends ChannelServiceGrpc.ChannelServiceImplBase {
             responseObserver.onCompleted();
 
         }
-        catch (Exception e) {
+        catch (StatusRuntimeException e) {
             responseObserver.onError(e);
         }
-    }
-
-    @Override
-    public void getLastMessage(ChannelId channelId, StreamObserver<Message> responseObserver) {
-        try {
-            Message message = coreClient.getLastMessage(new BigInteger(channelId.getUserId()),
-                new BigInteger(channelId.getId()));
-
-            responseObserver.onNext(message);
-            responseObserver.onCompleted();
-
-        }
         catch (Exception e) {
-            responseObserver.onError(e);
-        }
-    }
-
-    @Override
-    public void getAllMessages(ChannelId channelId, StreamObserver<MessageList> responseObserver) {
-        try {
-            List<Message> messages = coreClient.getAllMessages(new BigInteger(channelId.getUserId()),
-                new BigInteger(channelId.getId()));
-
-            MessageList list = MessageList.newBuilder().addAllMessages(messages).build();
-            responseObserver.onNext(list);
-            responseObserver.onCompleted();
-
-        }
-        catch (Exception e) {
-            responseObserver.onError(e);
+            responseObserver.onError(
+                    Status.INTERNAL
+                            .withDescription("Unexpected server error: " + e.getMessage())
+                            .withCause(e)
+                            .asRuntimeException()
+            );
         }
     }
 }
