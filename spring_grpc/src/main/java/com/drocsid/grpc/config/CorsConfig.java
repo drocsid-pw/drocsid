d7f6440c7136f4f0efd3ae47a7c25a6c9857452a
Jakub Mieczkowski <jmieczkowski04@gmail.com>
2026-01-18 23:40:00 +0100

    feat: better config

--- spring_grpc/src/main/java/com/drocsid/grpc/config/CorsConfig.java
+++ spring_grpc/src/main/java/com/drocsid/grpc/config/CorsConfig.java
@@ -14,9 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:3000",
-                                "http://localhost:5173"
+                        .allowedOriginPatterns(
+                                "*"
                         )
                         .allowedMethods(
                                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
